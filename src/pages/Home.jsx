0c7e84f9763cbf5b35691e2e54b4c8c52d36d47e
karan <karanthakurr2001@gmail.com>
2024-06-26 18:07:39 +0530

    Added the home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-70">
+        <h1 className="text-3xl">Thanks for trying Ollum</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
