fd71904d6f45e441da4b01a294b6c23a55d8c83f
karan <karanthakurr2001@gmail.com>
2024-06-27 17:35:43 +0530

    added the eye toggle button and enhance styling in In and signUp

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,45 +1,58 @@
 import React, { useState } from "react";
 import backgroundImage from "../assets/bg_image.png";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 const SignIn = () => {
-  const handleSubmit = () => {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent form submission for demonstration
     console.log("submitted");
   };
-  const [password, setPassword] = useState("");
+
   return (
     <div
-      className="flex flex-col justify-center items-center h-screen bg-gray-100 bg-blend-overlay "
+      className="flex flex-col justify-center items-center h-screen bg-gray-100 bg-blend-overlay"
       style={{
         backgroundImage: `url(${backgroundImage})`,
       }}
     >
       <div className="w-90 p-4 bg-white rounded-lg shadow-lg">
         <h2 className="text-xl font-bold mb-4 text-red-600">Sign In</h2>
-        <form className="space-y-4">
-          <div className="relative-flex ">
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="flex items-center border border-gray-300 rounded-sm">
             <input
-              className="w-100 form-control px-3 py-2 border border-gray-300 border-r-0 rounded-sm focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-2 border-none focus:outline-none"
               type="email"
               placeholder="Email"
             />
-            <span className=" leading-3 px-3 py-2 bg-gray-300 border-gray-300 border-r-2">
+            <span className="px-3 py-2 bg-gray-300 rounded-sm">
               @testingXperts.com
             </span>
           </div>
-          <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-500"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-
+          <div className="flex items-center border border-gray-300 rounded-sm">
+            <input
+              className="w-full px-3 py-2 border-none focus:outline-none"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="p-2 border outline-none border-none rounded-r-sm"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <MdOutlineRemoveRedEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
           <button
             type="submit"
             className="w-full bg-blue-500 text-white py-2 px-4 rounded-sm hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-            onClick={handleSubmit}
           >
-            Sign Up
+            Sign In
           </button>
         </form>
       </div>
--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -1,68 +1,81 @@
 import React, { useState } from "react";
 import backgroundImage from "../assets/bg_image.png";
 import { useToast } from "@chakra-ui/react";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const toast = useToast();
-  const handleSubmit = (e) => {
-    if(password !== confirmPassword){
-      return toast({
-        title: 'Something went wrong',
-          description: "password and confirm password should be same.",
-          status: 'warning',
-          duration: 5000,
-          isClosable: true,
-      })
-    }
-    e.preventDefault();
-    console.log("form submitted")
-  }
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const toast = useToast();
+  const handleSubmit = (e) => {
+    if (password !== confirmPassword) {
+      return toast({
+        title: "Something went wrong",
+        description: "password and confirm password should be same.",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    e.preventDefault();
+    console.log("form submitted");
+  };
   return (
     <div
-      className="flex flex-col justify-center items-center h-screen bg-gray-100 bg-blend-overlay "
+      className="flex flex-col justify-center items-center h-screen bg-gray-100 bg-blend-screen"
       style={{
         backgroundImage: `url(${backgroundImage})`,
       }}
     >
-      <div className="w-85 p-4 bg-white rounded-lg shadow-lg">
+      <div className="w-90 p-4 bg-white rounded-lg shadow-lg">
         <h2 className="text-xl font-bold mb-4 text-red-600">Sign Up</h2>
-        <form className="space-y-4">
-          {/* <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-500"
-            type="text"
-            placeholder="Username"
-            defaultValue={'@testingXperts.com'}
-          /> */}
-          <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-500"
-            type="email"
-            placeholder="Email"
-            defaultValue={"@testingXperts.com"}
-          />
-          <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-500"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-          <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-500"
-            type="password"
-            placeholder="Confirm Password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-          />
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="flex items-center border border-gray-300 rounded-sm">
+            <input
+              className="w-full px-3 py-2 border-none focus:outline-none"
+              type="email"
+              placeholder="Email"
+            />
+            <span className="px-3 py-2 bg-gray-300 rounded-sm">
+              @testingXperts.com
+            </span>
+          </div>
+          <div className="flex items-center border border-gray-300 rounded-sm ">
+            <input
+              className="w-full px-3 py-2 border-none focus:outline-none"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div className="flex items-center border border-gray-300 rounded-sm">
+            <input
+              className="w-full px-3 py-2 border-none focus:outline-none"
+              type={showPassword ? "text" : "password"}
+              placeholder="confirm password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="p-2 border outline-none border-none rounded-r-sm"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <MdOutlineRemoveRedEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
           <button
             type="submit"
             className="w-full bg-blue-500 text-white py-2 px-4 rounded-sm hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-            onClick={handleSubmit}
           >
             Sign Up
           </button>
         </form>
+        <Link to={"/signin"}>
+          <span className="text-green-600 text-xs">Already have an account?</span>
+        </Link>
       </div>
     </div>
   );
