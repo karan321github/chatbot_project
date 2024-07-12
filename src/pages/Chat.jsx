138f369f69caacea3afc916ac424f9d1d59957e9
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-07-12 10:01:17 +0530

    some modification in chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,15 +1,16 @@
-import React from "react";
 import Header from "../components/Header";
 import SideWrapper from "../components/SideWrapper";
 import ChatBody from "../components/ChatBody";
 import styles from "./Chat.module.css";
+import { selectIsLoggedIn } from "../store/reducers/userSlice";
 
 const Chat = () => {
+  const isLoggedIn = selectIsLoggedIn;
   return (
     <div className={styles.main}>
       <Header />
       <div className={styles.content}>
-        <SideWrapper />
+        {isLoggedIn && <SideWrapper />}
         <ChatBody />
       </div>
     </div>
