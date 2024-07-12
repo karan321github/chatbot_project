23e106c97cd2068cce44c9f07d9d58645c72654a
karan <karanthakurr2001@gmail.com>
2024-07-12 16:03:27 +0530

    update the styling of chat body section

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducers from './reducers/userSlice.js'
-
-export const store  = configureStore({
-    reducer: {
-        authReducers
-    } 
-})
