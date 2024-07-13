ee009d67b238556c803c3526b4e90c8920d8308f
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-07-14 01:43:13 +0530

    after all updating the chat.jsx page

--- chatbot_project/src/pages/Chat.jsx
+++ chatbot_project/src/pages/Chat.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import SideWrapper from "../components/SideWrapper";
 import ChatBody from "../components/ChatBody";
@@ -5,12 +6,23 @@ import styles from "./Chat.module.css";
 import { selectIsLoggedIn } from "../store/reducers/userSlice";
 
 const Chat = () => {
+  const [isSideWrapperOpen, setIsSideWrapperOpen] = useState(false);
   const isLoggedIn = selectIsLoggedIn;
+
+  const toggleSideWrapper = () => {
+    setIsSideWrapperOpen(!isSideWrapperOpen);
+  };
+
   return (
     <div className={styles.main}>
-      <Header />
+      <Header isSideWrapperOpen={isSideWrapperOpen} />
       <div className={styles.content}>
-        {isLoggedIn && <SideWrapper />}
+        {isLoggedIn && (
+          <SideWrapper
+            isOpen={isSideWrapperOpen}
+            toggleSideWrapper={toggleSideWrapper}
+          />
+        )}
         <ChatBody />
       </div>
     </div>
